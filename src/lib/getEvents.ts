1db7ee179efb6a9edbe603565f611912d6780719
silvacdp <silvacdp@gmail.com>
2025-07-10 14:24:52 -0400

    Update getEvents.ts
    correcting field names

--- src/lib/getEvents.ts
+++ src/lib/getEvents.ts
@@ -19,11 +19,25 @@ export async function getEvents() {
   }
   
   const data = await res.json();
-  return data.records.map(record => ({
-    slug: record.fields.Slug,
-    title: record.fields.Title,
-    date: record.fields.Date,
-    location: record.fields.Location,
-    summary: record.fields.Summary,
+return data.records.map(record => ({
+  slug: record.fields.Title
+    .toLowerCase()                    // "hurricane katrina 2005"
+    .replace(/\s+/g, '-')            // "hurricane-katrina-2005"
+    .replace(/[^a-z0-9-]/g, '')      // removes any special characters
+    .replace(/--+/g, '-')            // replaces multiple dashes with single dash
+    .replace(/^-|-$/g, ''),          // removes dashes from start/end
+  title: record.fields.Title,
+  location: record.fields.Location,
+  startDate: record.fields['Start Date'],
+  endDate: record.fields['End Date'],
+  category: record.fields.Category,
+  tags: record.fields.Tags,
+  quote: record.fields.Quote,
+  summary: record.fields.Summary,
+  imageUrl: record.fields['Image URL'],
+  imageSource: record.fields['Image Source'],
+  links: record.fields.Links,
+  published: record.fields.Published,
+  misc: record.fields.Misc,
   }));
 }
