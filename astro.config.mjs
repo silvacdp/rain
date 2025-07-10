401dac4607d904f9fa31f9de4390f3ee9df2f61c
silvacdp <silvacdp@gmail.com>
2025-07-10 12:27:39 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,20 @@
+/*
 import { defineConfig } from 'astro/config';
 import dotenv from 'dotenv';
 dotenv.config();
 
 export default defineConfig({
   // your config here
+});
+*/
+
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+
+export default defineConfig({
+  site: 'https://your-site.com',
+  output: 'static',
+  integrations: [
+    svelte()
+  ],
 });
