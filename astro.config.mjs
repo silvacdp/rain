afed62f6707c3e54ed5061fbc7d83c8a226c4af5
silvacdp <silvacdp@gmail.com>
2025-07-10 22:30:51 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 
 export default defineConfig({
-  site: 'https://your-site.com',
+  site: 'https://morerain.vercel.app',
   output: 'static',
   integrations: [
     svelte()
